18692aee840fd0263ee9cca6f6e2055f6b621503
raurir <raurir@gmail.com>
2015-02-12 09:42:18 +1100

    Deploying meandering rotation and dotted lines

--- js/meandering_polygons.js
+++ js/meandering_polygons.js
@@ -1,6 +1,5 @@
 (function() {
 var sw = 1000, sh = 1000;
-var fgColour = colours.getRandomColour();
 var bgColour = colours.getNextColour();
 var dots = 24;
 var arrDots = [];
@@ -11,6 +10,14 @@ var ctx = bmp.ctx;
 
 var range = 0.2;
 
+function white() {
+  var b = ~~(230 + Math.random() * 25),
+    a = Math.round((0.7 + Math.random() * 0.3) * 100) / 100,
+    w = "rgba("+b+","+b+","+b+","+a+")";
+    // con.log(w);
+  return w;
+}
+
 function init() {
   var j = 0
   while(j < dots) {
@@ -19,39 +26,39 @@ function init() {
       y: Math.random(),
       fx: 0,
       fy: 0,
-      vx: Math.random(),
-      vy: Math.random(),
+      vx: Math.random() * 0.001,
+      vy: Math.random() * 0.001,
       dir: Math.random() * Math.PI * 2,
       dirFloat: 0,
-      colour: colours.getRandomColour(),
-      connected: [],
-
+      rotation: 0,
+      rotationFloat: 0,
       type: ~~(Math.random() * 2),
       bmp: null,
       size: 0,
       generate: function() {
         this.size = 6 + ~~(Math.random() * 20);
         var bmp = dom.canvas(this.size, this.size);
-        document.body.appendChild(bmp.canvas);
+        // document.body.appendChild(bmp.canvas);
         var ctx = bmp.ctx;
+        // ctx.fillStyle = "#f00";
+        // ctx.fillRect(0, 0, this.size, this.size);
+
         switch(this.type) {
           case 0 : // double circle
-            // ctx.fillStyle = "#f00";
-            // ctx.fillRect(0, 0, this.size, this.size);
 
-            var lineWidth = Math.random() * 3;
+            var lineWidth = Math.random() * 2;
             var radius = this.size / 2 - lineWidth;
 
             ctx.beginPath();
             ctx.lineWidth = lineWidth;
-            ctx.strokeStyle = "#fff";
+            ctx.strokeStyle = white()
             ctx.drawCircle(this.size / 2, this.size / 2, radius);
             ctx.stroke();
 
             radius *= Math.random();
 
             ctx.beginPath();
-            ctx.fillStyle = "#fff";
+            ctx.fillStyle =  white();
             ctx.drawCircle(this.size / 2, this.size / 2, radius);
             ctx.fill();
 
@@ -60,7 +67,7 @@ function init() {
             var sides = 3 + ~~(Math.random() * 7);
             ctx.beginPath();
             ctx.lineWidth = 2;
-            ctx.strokeStyle = "#fff";
+            ctx.strokeStyle = white();
             ctx.drawCircle(this.size / 2, this.size / 2, radius - 1);
             for(var i = 0; i < sides; i++) {
               var angle = i / sides * Math.PI * 2 ;
@@ -79,36 +86,49 @@ function init() {
           case 2 :
             break;
         }
-        this.bmp = bmp.canvas;
+        this.bmp = bmp;
       },
       draw: function() {
         if (this.bmp == null) {
           this.generate();
         }
-        // con.log('draw', this.bmp);
-        ctx.drawImage(this.bmp, this.x * sw - this.size / 2, this.y * sh - this.size / 2);
+        ctx.save();
+        ctx.translate(this.x * sw, this.y * sh);
+        ctx.rotate(this.rotation);
+        ctx.drawImage(this.bmp.canvas, -this.size / 2, - this.size / 2);
+        ctx.restore();
       },
+
+      attraction: -0.00001,
+      speed: 0.0001,
+      friction: 0.9,
+
 
       move: function() {
 
-        // return;
+        this.rotationFloat += (Math.random() > 0.5 ? -1 : 1) * 0.5;
+        this.rotation -= (this.rotation - this.rotationFloat) * 0.01;
+
         this.dirFloat += (Math.random() > 0.5 ? -1 : 1) * 0.1;
         this.dir -= (this.dir - this.dirFloat) * 0.01;
-        this.vx = Math.sin(this.dir) * 0.001 + this.fx;
-        this.vy = Math.cos(this.dir) * 0.001 + this.fy;
+
+        this.vx += Math.sin(this.dir) * this.speed + this.fx;
+        this.vy += Math.cos(this.dir) * this.speed + this.fy;
+        this.vx *= this.friction;
+        this.vy *= this.friction;
+
         this.x += this.vx;
         this.y += this.vy;
 
-        if (this.x < -0.1) this.x = 1.1;
-        if (this.x > 1.1) this.x = -0.1;
-        if (this.y < -0.1) this.y = 1.1;
-        if (this.y > 1.1) this.y = -0.1;
+        if (this.x < -0.2) this.x = 1.2;
+        if (this.x > 1.2) this.x = -0.2;
+        if (this.y < -0.2) this.y = 1.2;
+        if (this.y > 1.2) this.y = -0.2;
       },
-      force: function(opposite, distance) {
-
-        return;
-        this.fx = (this.x - opposite.x) / distance * 0.1 * (range - distance);
-        this.fy = (this.y - opposite.y) / distance * 0.1 * (range - distance);
+      force: function(opposite, distance, deltaX, deltaY) {
+        // return;
+        this.fx = deltaX / distance * this.attraction; // * (range - distance);
+        this.fy = deltaY / distance * this.attraction; // * (range - distance);
       }
 
     }
@@ -134,6 +154,7 @@ function init() {
           // ctx.strokeStyle = "#f0f";// dot.colour;
           // ctx.lineCap = 'round';
           // ctx.moveTo(mx * sw, my * sh);
+
           // ctx.lineTo(dot.x * sw, dot.y * sh);
           // ctx.stroke();
           ctx.beginPath();
@@ -161,37 +182,26 @@ function init() {
 
 }
 
-var groups = [];
 var lines = [];
-var t = 0;
 function uniqueId(j, k) {
-  // return j + ":" + k;
   return ((j*(j-1))/2) + k;
 }
 
 for (var j = 0; j < dots; j++) {
-  // lines[j] = [];
   for (var k = 0;k < j; k++) {
-    // lines[j][k] = {};
-    // var uniqueId = j + k ;
-
     lines[uniqueId(j,k)] = {
       points: [j,k],
-      val:0
+      lineWidth: 0,
+      colour: white(),
+      dashes: ~~(Math.random() * 5)
     };
-
-    // con.log(k, j, t);
-    t++
   }
-  // con.log("=============");
 }
-// con.log(lines);
+
 
 function render() {
-  ctx.fillStyle = "rgba(200,200,180,0.9)"; //bgColour;
+  ctx.fillStyle = "rgba(180,180,200,1)";
   ctx.fillRect(0, 0, sw, sh);
-
-
 
   for (var j = 0; j < dots; j++) {
 
@@ -211,166 +221,87 @@ function render() {
         d = Math.sqrt(dx * dx + dy * dy);
 
       var inRange = d < range;
-
       // ignore off screen
       if (other.x < 0 || other.x > 1 || other.y < 0 || other.y > 1) inRange = false;
 
-
-
       if (inRange) {
 
-        dot.force(other, d);
-        other.force(dot, d);
+        dot.force(other, d, dx, dy);
+        other.force(dot, d, dx, dy);
 
-        /*
-        var groupIndex = -1, jFound = false, kFound = false;
+        lines[ lineId ].lineWidth -= (lines[ lineId ].lineWidth - 3) * 0.01;
 
-        var bothFound = false;
+      } else {
 
-        for (var g = 0; g < groups.length; g++) {
+        lines[ lineId ].lineWidth *= 0.9;
+        if (lines[ lineId ].lineWidth < 0.1) {
+          lines[ lineId ].lineWidth = 0;
+        }
+      }
 
-          jFound = groups[g].indexOf(j) > -1,
-          kFound = groups[g].indexOf(k) > -1;
+    }
+  }
 
-          if (jFound && kFound) {
-            groupIndex = g;
-            bothFound = true
-            bothIndex = g;
-          } else if (jFound) {
-            groupIndex = g;
-          } else if (kFound) {
-            groupIndex = g;
-          } else {
-            // groups[g].push(j,k);
+
+  for (var m = 0, ml = lines.length; m < ml; m++) {
+    var lineM = lines[m];
+      pointsM = lineM.points,
+      a = arrDots[ pointsM[0] ],
+      b = arrDots[ pointsM[1] ];
+
+    if (lineM.lineWidth) {
+
+      for (var k = 0; k < m; k++) {
+        var lineK = lines[k];
+        if (lineK.lineWidth) {
+          var pointsK = lineK.points,
+            c = arrDots[ pointsK[0] ],
+            d = arrDots[ pointsK[1] ];
+          var intersects = intersection(a, b, c, d);
+          if (intersects) {
+            debugCircle(intersects, lineM.colour);
           }
-        };
-
-        if (jFound && kFound) {
-
-        } else if (jFound) {
-          con.log("groupIndex jFound", groups[groupIndex])
-          if (groups[groupIndex].length >= 3) {
-            con.log("creating new group!", groupIndex)
-            groupIndex++;
-            groups[groupIndex] = [j,k];
-          } else {
-            groups[groupIndex].push(k);
-          }
-
-        } else if (kFound) {
-          con.log("groupIndex kFound", groups[groupIndex])
-
-          if (groups[groupIndex].length >= 3) {
-            con.log("creating new group!")
-            groupIndex++;
-            groups[groupIndex] = [j,k];
-          } else {
-            groups[groupIndex].push(j);
-          }
-
-        } else {
-          groups[groups.length] = [j,k];
         }
 
-
-        // if (other.connected.indexOf(j) == -1) {
-        //   con.log("adding ")
-        //   other.connected.push(j);
-        //   dot.connected.push(k);
-        // }
-        */
-
-        // drawLine(dot, other);
-        // lines.push([dot,other])
-        lines[ lineId ].val -= (lines[ lineId ].val - 3) * 0.01;
-      } else {
-        // con.log("not in range");
-        lines[ lineId ].val *= 0.99;
       }
-
+      drawLine(a, b, lineM);
 
     }
+
   }
 
-/*
-  for (var g = 0; g < groups.length; g++) {
-    // con.log(g, groups[g].length)
-    ctx.beginPath();
-
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = "#f0f";// dot.colour;
-    ctx.lineCap = 'round';
-    ctx.fillStyle = "rgba(255,0,0,0.6)";
-    for (var c = 0; c < groups[g].length; c++) {
-      var dot = arrDots[ groups[g][c] ];
-      var xp = dot.x * sw, yp = dot.y * sh;
-      if (c == 0){
-        ctx.moveTo(xp, yp);
-      } else {
-        ctx.lineTo(xp, yp);
-      }
-    }
-    ctx.closePath();
-    ctx.fill();
-    ctx.stroke();
-  }
-*/
-
-  // for (var m = 0, ml = lines.length; m < ml; m++) {
-    // var lineM = lines[m];
-    // con.log(lines,m, lineM);
-    // for (var k = 0; k < m; k++) {
-    //   var lineK = lines[k];
-    //   var intersects = intersection(lineM[0], lineM[1], lineK[0], lineK[1]);
-    //   if (intersects) {
-    //     debugCircle(intersects);
-    //   }
-    // }
-    // drawLine(lineM[0], lineM[1]);
-
-  for (var m in lines) {
-    var line = lines[m],
-      points = line.points,
-      a = arrDots[ points[0] ],
-      b = arrDots[ points[1] ];
-    // if (line.val) {
-    drawLine(a, b, line.val);
-    // }
-  }
 
   requestAnimationFrame(render);
 }
 
 
 
-function drawLine(a, b, lineWidth) {
-  if (lineWidth > 0) {
+function drawLine(a, b, line) {
+  if (line.lineWidth > 0) {
   // con.log(lineWidth);
+    ctx.save();
     ctx.beginPath();
-    ctx.lineWidth = lineWidth;
-    ctx.strokeStyle = "#fff";//dot.colour;
+    ctx.lineWidth = line.lineWidth;
+    ctx.strokeStyle = line.colour;
+    if (line.dashes > 2) ctx.setLineDash([line.dashes]);
     ctx.lineCap = 'round';
     ctx.moveTo(a.x * sw, a.y * sh);
     ctx.lineTo(b.x * sw, b.y * sh);
     ctx.stroke();
+    ctx.restore();
   }
 }
 
 
-
-
-
-
-function debugCircle(dot) {
-  return;
+function debugCircle(dot, colour) {
   var radius = sw * range / 2;
   // ctx.beginPath();
   // ctx.fillStyle = "rgba(255,0,0,0.3)"; // dot.colour;
   // ctx.drawCircle(dot.x * sw, dot.y * sh, radius);
   // ctx.fill();
-  var radius = 5;
+  var radius = 3;
   ctx.beginPath();
-  ctx.fillStyle = "#f00" // dot.colour;
+  ctx.fillStyle = colour;
   ctx.drawCircle(dot.x * sw, dot.y * sh, radius);
   ctx.fill();
 }
