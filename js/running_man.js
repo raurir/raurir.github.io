af2c3d635e43afdf217dafed1f713b1552573cd2
Rauri Rochford <raurir@gmail.com>
2015-06-14 16:55:25 +1000

    Deploying conflict

--- js/running_man.js
+++ js/running_man.js
@@ -1,7 +1,6 @@
 var con = console;
 var isNode = (typeof module !== 'undefined');
 
-<<<<<<< HEAD
 var limbs = {
 	"thigh": {
 		"range": 1,
@@ -23,8 +22,6 @@ var limbs = {
 	}
 };
 
-=======
->>>>>>> cfbc399014f8293c0cbc325710bb65d58abaa0a2
 var running_man = (function() {
 
 	var sw = 600, sh = 600;
@@ -36,17 +33,12 @@ var running_man = (function() {
 	var cy = sh * 1/ 4;
 
 	var editor = dom.element("div", {style: {position: "absolute", top: 10, left: 10}});
-<<<<<<< HEAD
 	var output = dom.element("pre", {style: {color: "white","font-size":"10px", }});
-=======
-	var output = dom.element("pre", {style:{color: "white","font-size":"10px", }});
->>>>>>> cfbc399014f8293c0cbc325710bb65d58abaa0a2
 
 
 	function createLimb(options) {
 
 		var parent = options.parent;
-<<<<<<< HEAD
 
 		return {
 			pos: {},
@@ -54,22 +46,6 @@ var running_man = (function() {
 				var osc = options.movement.baserot + Math.sin(time + options.movement.offset + options.phase) * options.movement.range;
 				var pos = this.position(osc);
 				return Math.max(cy + pos.sy, cy + pos.ey);
-=======
-		var fn = options.fn;
-		var offset = options.offset;
-
-		return {
-			pos: {},
-			render: function(time) {
-				var osc = options.movement.baserot + fn(time + offset) * options.movement.range;
-				var pos = this.position(osc);
-				ctx.beginPath();
-				ctx.strokeStyle = "#0f0";
-				ctx.moveTo(cx + pos.sx, cy + pos.sy);
-				ctx.lineTo(cx + pos.ex, cy + pos.ey);
-				ctx.stroke();
-				ctx.closePath();
->>>>>>> cfbc399014f8293c0cbc325710bb65d58abaa0a2
 			},
 			position: function(osc) {
 				if (osc) {
@@ -86,7 +62,6 @@ var running_man = (function() {
 						pos.ex += parentPos.ex;
 						pos.ey += parentPos.ey;
 					}
-<<<<<<< HEAD
 					this.pos = pos;
 				}
 				return this.pos;
@@ -105,43 +80,15 @@ var running_man = (function() {
 				ctx.drawCircle(cx + this.pos.ex, max + cy + this.pos.ey, 5);
 				ctx.closePath();
 				ctx.fill();
-=======
-					// con.log(pos);
-					this.pos = pos;
-					return pos;
-				} else {
-					return this.pos;
-				}
->>>>>>> cfbc399014f8293c0cbc325710bb65d58abaa0a2
 			}
 		}
 	}
 
-<<<<<<< HEAD
 	function settings() {
 		output.innerHTML = ("var limbs = " + JSON.stringify(limbs, null, "\t") + ";");
 	};
 	settings();
 	
-=======
-var limbs = {
-	"thigh": {
-		"range": 1,
-		"baserot": 0.5,
-		"length": 90
-	},
-	"calf": {
-		"range": -1,
-		"baserot": -0.9,
-		"length": 90
-	},
-	"foot": {
-		"range": -0.5,
-		"baserot": 1.6,
-		"length": 20
-	}
-};
->>>>>>> cfbc399014f8293c0cbc325710bb65d58abaa0a2
 
 	function createEditor(l,k) {
 		var edit = dom.element("div");
@@ -152,11 +99,7 @@ var limbs = {
 		edit.appendChild(input);
 		input.addEventListener("change", function(e) {
 			limbs[l][k] = parseFloat(e.target.value);
-<<<<<<< HEAD
 			settings();
-=======
-			output.innerHTML = ("var limbs = " + JSON.stringify(limbs, null, "\t") + ";");
->>>>>>> cfbc399014f8293c0cbc325710bb65d58abaa0a2
 		})
 	}
 
@@ -169,23 +112,12 @@ var limbs = {
 	editor.appendChild(output);
 
 
-<<<<<<< HEAD
 	var t1 = createLimb({parent: null, movement: limbs.thigh, phase: 0});
 	var c1 = createLimb({parent: t1, movement: limbs.calf, phase: 0});
 	var f1 = createLimb({parent: c1, movement: limbs.foot, phase: 0});
 	var t2 = createLimb({parent: null, movement: limbs.thigh, phase: Math.PI});
 	var c2 = createLimb({parent: t2, movement: limbs.calf, phase: Math.PI});
 	var f2 = createLimb({parent: c2, movement: limbs.foot, phase: Math.PI});
-=======
-
-
-	var t1 = createLimb({parent: null, movement: limbs.thigh, fn: Math.sin, offset: 0});
-	var c1 = createLimb({parent: t1, movement: limbs.calf, fn: Math.cos, offset: 0});
-	var f1 = createLimb({parent: c1, movement: limbs.foot, fn: Math.sin, offset: 0});
-	var t2 = createLimb({parent: null, movement: limbs.thigh, fn: Math.sin, offset: Math.PI});
-	var c2 = createLimb({parent: t2, movement: limbs.calf, fn: Math.cos, offset: Math.PI});
-	var f2 = createLimb({parent: c2, movement: limbs.foot, fn: Math.sin, offset: Math.PI});
->>>>>>> cfbc399014f8293c0cbc325710bb65d58abaa0a2
 
 	function render(t) {
 		// var time = 50;
@@ -195,7 +127,6 @@ var limbs = {
 		// ctx.fillStyle = "#0f0";
 		// ctx.fillRect(cx - 2, cy - 2, 4, 4);
 
-<<<<<<< HEAD
 		// cy = 200 - Math.abs(Math.sin(time) * 50);
 		var max = 0;
 		t1.calc(time);
@@ -220,15 +151,6 @@ var limbs = {
 
 
 
-=======
-		t1.render(time);
-		c1.render(time);
-		f1.render(time);
-
-		t2.render(time);
-		c2.render(time);
-		f2.render(time);
->>>>>>> cfbc399014f8293c0cbc325710bb65d58abaa0a2
 
 		requestAnimationFrame(render);
 	}
--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 require([
-<<<<<<< HEAD
 	"js/dom.js",
 	"js/rand.js",
 	"js/geom.js",
@@ -8,18 +7,4 @@ require([
 	"js/experiments.js",
 	"js/experiments_progress.js"], function(dom, rand, geom, colours, experiment_factory, experiments, experiments_progress) {
 		console.log("experiments main ready");
-=======
-	"js/dom.js", 
-	"js/rand.js", 
-	"js/geom.js", 
-	"js/colours.js",
-	"js/experiment_factory.js", 
-	"js/experiments.js", 
-	"js/experiments_progress.js"], function(dom, rand, geom, colours, experiment_factory, experiments, experiments_progress) {
-		console.log("done");
-    //This function is called when scripts/helper/util.js is loaded.
-    //If util.js calls define(), then this function is not fired until
-    //util's dependencies have loaded, and the util argument will hold
-    //the module value for "helper/util".
->>>>>>> cfbc399014f8293c0cbc325710bb65d58abaa0a2
 });
