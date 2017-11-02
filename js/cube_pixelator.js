88b33106b2ceb4e08e93239e877c7955cb333b19
Rauri Rochford <raurir@gmail.com>
2017-11-02 12:57:08 +1100

    Deploying pixelator

--- js/cube_pixelator.js
+++ js/cube_pixelator.js
@@ -1,11 +1,11 @@
 define("cube_pixelator", [], function() {
 
-	const pixels = 32;
-	const cubeSize = 16;
-	const gridSize = 24;
+	const pixels = 48;
+	const cubeSize = 8;
+	const gridSize = 10;
 
 	var camera, scene, renderer;
-	var mouse = {x: 0, y: 0, toggle: true};
+	var mouse = {x: 0, y: 0};
 	var camPos = {x: 0, y: 0, z: 0};
 	var sw = window.innerWidth, sh = window.innerHeight;
 	var holder;
@@ -17,19 +17,26 @@ define("cube_pixelator", [], function() {
 	var currentImage = 0;
 
 	function cube(w, h, d, colour) {
+		const geometry = new THREE.PlaneGeometry(w, h, 1);
 		const material = new THREE.MeshLambertMaterial({color: colour});
-		const geometry = new THREE.BoxGeometry(w, h, d);
+		// const material = new THREE.MeshLambertMaterial({color: colour});
+		// const geometry = new THREE.BoxGeometry(w, h, d);
 		return new THREE.Mesh(geometry, material);
 	}
 
 	function init() {
+
+		for (var p = 0; p < pixels * pixels; p++) {
+			rotations.push({rotation: Math.random()});
+		}
+
 		// yes i should use promises, thanks!
 		const fn1 = () => createBitmap("cash", fn2);
 		const fn2 = () => createBitmap("marilyn", fn3);
 		const fn3 = () => createBitmap("hicks", fn4);
 		const fn4 = () => {
-			rotations = images[currentImage].slice();
-			createScene();
+			// rotations = images[currentImage].slice();
+			setTimeout(createScene, 200); // createScene is expensive
 		}
 		fn1();
 	}
@@ -50,11 +57,12 @@ define("cube_pixelator", [], function() {
 			var pixelData = ctx.getImageData(0, 0, pixels, pixels).data;
 			for (var p = 0; p < pixelData.length; p+=4) {
 				var r = pixelData[p];
-				grayscale.push(r / 255);
+				grayscale.push({rotation: r / 255});
 			}
 			images.push(grayscale);
 			// con.log("grayscale", image, grayscale.length)
-			resolve();
+			progress("render:progress", images.length / 4);
+			setTimeout(resolve, 100);
 		}
 		img.src = `./assets/${image}.jpg`;
 		// document.body.appendChild(img);
@@ -69,19 +77,15 @@ define("cube_pixelator", [], function() {
 		scene.add(camera);
 
 		var lightAbove = new THREE.DirectionalLight(0xffffff, 1);
-		lightAbove.position.set(0, 1, 0);
+		lightAbove.position.set(0, 1, 0.5);
 		scene.add(lightAbove);
 
-		var lightLeft = new THREE.DirectionalLight(0xff0000, 0.1);
-		lightLeft.position.set(-1, 0, 0);
+		var lightLeft = new THREE.DirectionalLight(0xf0e5a1, 0.5);
+		lightLeft.position.set(-1, 0.5, 0.5);
 		scene.add(lightLeft);
 
-		var lightRight = new THREE.DirectionalLight(0x0000ff, 0.1);
-		lightRight.position.set(1, 0, 0);
-		scene.add(lightRight);
-
-		var lightBelow = new THREE.DirectionalLight(0xff00ff, 0.1);
-		lightBelow.position.set(0, -1, 0);
+		var lightBelow = new THREE.DirectionalLight(0x303030, 0.2);
+		lightBelow.position.set(0, -1, 0.25);
 		scene.add(lightBelow);
 
 		renderer = new THREE.WebGLRenderer({antialias: true});
@@ -112,41 +116,46 @@ define("cube_pixelator", [], function() {
 		document.body.appendChild(renderer.domElement);
 		render(0);
 		setInterval(toggleImage, 6000);
+
+		progress("render:complete", renderer.domElement);
 	}
 
 	const toggleImage = () => {
 		currentImage ++;
 		currentImage %= images.length;
+		con.log("toggleImage", currentImage)
 		var newRotations = images[currentImage].slice();
-		newRotations.easing = Quad.easeInOut;
-		// con.log("toggleImage", currentImage, images[currentImage][0]);
-		TweenMax.to(rotations, 3, newRotations);
+		for (var i = 0; i < pixels * pixels; i++) {
+			TweenMax.to(rotations[i], 3, {
+				rotation: newRotations[i].rotation,
+				easing: Back.easeInOut,
+				delay: i * 0.001
+			});
+		}
 	}
 
 	function render(time) {
 		grid.forEach((c, index) => {
-			c.rotation.y += c.rotateSpeed * 0.01;
+			// c.rotation.y += c.rotateSpeed * 0.01;
 			//c.rotateSpeed -= 0.01;
 			// c.rotation.x = 0 - c.rotateAmount * Math.PI / 4;
-			c.rotation.x = 0 - rotations[index] * Math.PI / 4;
+			c.rotation.x = Math.PI / 4 - rotations[index].rotation * Math.PI / 3;
 		});
 		// holder.rotation.x += Math.PI * 0.005;
 		// camPos.z = 100; //ortho camera
-		camPos.z = 400 + Math.sin(time * 0.0003) * 300;
-		camPos.y = -cubeSize * pixels + Math.sin(time * 0.00017) * 200;
-		camPos.x = 0 + Math.sin(time * 0.00012) * 200;
+		camPos.x = 0 + Math.sin(time * 0.00012) * 50;
+		// camPos.y = -cubeSize * pixels + Math.sin(time * 0.00017) * 50;
+		camPos.y = 0 + Math.sin(time * 0.00017) * 50;
+		camPos.z = 400 + Math.sin(time * 0.0001) * 300;
+
 		camera.position.set(camPos.x, camPos.y, camPos.z);
 		camera.lookAt( scene.position );
 		renderer.render( scene, camera );
 		requestAnimationFrame(render);
-		// setTimeout(() => render(time+1), 200);
 	}
-
-
 
 	return {
 		init: init
 	}
 
 });
-
